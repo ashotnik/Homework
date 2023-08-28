70e2d0e59d9e0fbde2a5973c14b91ebc6336b4e7
AshotNik <ashot.nik777@gmail.com>
2023-08-29 03:50:09 +0400

    BigLIst

---
+++ list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+mylist=[10,1,3,20,45,50,60,1,6,8,45,54]
+
+def get_list(ml):
+    mmax=0
+    
+    for i in range(len(ml)):
+        cnt=0
+        for j in range(i+1,len(ml)):
+            if len(ml)==j:break
+            if ml[i]>=ml[j]:break
+            cnt+=1
+            a=i
+            b=j+1
+        if cnt>mmax:
+            mmax=cnt
+            biggestlist=ml[a:b]
+    return biggestlist        
+big_list=get_list(mylist) 
+print(big_list)
